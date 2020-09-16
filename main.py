920e816ce67e9cc27430e25b9ca12a31775fc82c
Manikandan-Raju-cat <raju_manikandan@cat.com>
2020-09-17 02:14:18 +0530

    added images

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ import time
 ET_path = r'C:\Program Files (x86)\Caterpillar\Electronic Technician\Etech.exe'  # type:  str
 Active_codes = r'Active_code.png'                                                # type: str
 Active_events = r'Active_event.png'                                              # type: str
-connect = r''                                                                    # type: str
-ok = r''
+connect = r'connect.png'                                                                    # type: str
+ok = r'ok.png'
+delay = 5
 
 def open_application(app_path):
     os.startfile(app_path)
+    time.sleep(delay)
     pyautogui.hotkey('winleft')
 
 
@@ -31,9 +33,12 @@ def take_screenshot(position):
 
 if __name__ == "__main__":
     open_application(ET_path)
-    time.sleep(5)
+    time.sleep(delay)
     connect_position = click_on_button(connect)
-    time.sleep(10)
+    time.sleep(2*delay)
     ok_position = click_on_button(ok)
-    time.sleep(10)
+    time.sleep(4*delay)
+    ok_position = click_on_button(Active_codes)
+    time.sleep(2*delay)
+
 
