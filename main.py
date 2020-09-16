e6c0a5508e2b870b46948f5002ff40d096922c00
Manikandan Raju <raju_manikandan@cat.com>
2020-09-17 02:56:58 +0530

    ET connection Working

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import os
 import time
 
 
-# import subprocess
-# import sys
-
-
 ET_path = r'C:\Program Files (x86)\Caterpillar\Electronic Technician\Etech.exe'  # type:  str
-Active_codes = r'Active_code.png'                                                # type: str
+Active_codes = r'act_code.png'                                                # type: str
 Active_events = r'Active_event.png'                                              # type: str
 connect = r'connect.png'                                                                    # type: str
 ok = r'ok.png'
+check = r'check_box.png'
+ECM_select = r'HVAC_ECM.png'
 delay = 5
+
 
 def open_application(app_path):
     os.startfile(app_path)
-    time.sleep(delay)
-    pyautogui.hotkey('winleft')
+    print("app_opened")
+    #time.sleep(2*delay)
+    #pyautogui.hotkey('winleft')
 
 
 def click_on_button(image_path):
     position = pyautogui.locateCenterOnScreen(image_path)
+    print(position)
     pyautogui.moveTo(position)
     pyautogui.click()
+    pyautogui.click()
+    pyautogui.click()
+
     return position
 
 
@@ -31,14 +35,25 @@ def take_screenshot(position):
     img = pyautogui.screenshot(region=(0, 0, 300, 400))
 
 
+'''
 if __name__ == "__main__":
-    open_application(ET_path)
-    time.sleep(delay)
-    connect_position = click_on_button(connect)
-    time.sleep(2*delay)
-    ok_position = click_on_button(ok)
-    time.sleep(4*delay)
-    ok_position = click_on_button(Active_codes)
-    time.sleep(2*delay)
-
+'''
+open_application(ET_path)
+time.sleep(2.5*delay)
+connect_position = click_on_button(connect)
+print("connect_clicked")
+time.sleep(8*delay)
+connect_position = click_on_button(ECM_select)
+print("ECM_Selected")
+time.sleep(1*delay)
+connect_position = click_on_button(ok)
+print("ok_clicked")
+time.sleep(2*delay)
+code_pos = click_on_button(Active_codes)
+print("code_clicked")
+time.sleep(2*delay)
+code_pos = click_on_button(check)
+print(type(code_pos))
+print("check_clicked")
+time.sleep(5*delay)
 
