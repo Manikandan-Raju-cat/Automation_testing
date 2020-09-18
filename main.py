172f71c4bf2e83378febf20925d7ef4bc981ec12
Manikandan-Raju-cat <raju_manikandan@cat.com>
2020-09-18 14:41:38 +0530

    Added Screenshot function

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import time
 
 
 ET_path = r'C:\Program Files (x86)\Caterpillar\Electronic Technician\Etech.exe'
-Active_codes = r'act_code.png'
-Active_events = r'Active_event.png'
-connect = r'connect.png'
-ok = r'ok.png'
-check = r'check_box.png'
-ECM_select = r'HVAC_ECM.png'
-log = r'log_code.png'
+Active_codes = r'C:\Users\rajum1\Documents\GitHub\Automation_testing\act_code.png'
+Active_events = r'C:\Users\rajum1\Documents\GitHub\Automation_testing\Active_event.png'
+connect = r'C:\Users\rajum1\Documents\GitHub\Automation_testing\connect.png'
+ok = r'C:\Users\rajum1\Documents\GitHub\Automation_testing\ok.png'
+check = r'C:\Users\rajum1\Documents\GitHub\Automation_testing\check_box.png'
+ECM_select = r'C:\Users\rajum1\Documents\GitHub\Automation_testing\HVAC_ECM.png'
 
 
 def open_application(app_path):
@@ -31,10 +30,12 @@ def click_on_button(image_path):
     return position
 
 
-def take_screenshot(position):
-    img = pyautogui.screenshot(region=(0, 0, 800, 600))
+def take_screen_shot(image_number):
+    path = r"C:\Projects\UAT\AD63\Test_results\images\result_"+str(image_number)+".png"
+    pyautogui.screenshot(path, region=(0, 0, 800, 600))
+    return path
 
-
+'''
 open_application(ET_path)
 time.sleep(10)
 
@@ -49,3 +50,7 @@ time.sleep(10)
 
 code_position = click_on_button(Active_codes)
 time.sleep(5)
+'''
+
+
+print(take_screen_shot(1))
