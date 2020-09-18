0f068b761a762d6a969fad0ab50759a2604eaf50
Manikandan-Raju-cat <raju_manikandan@cat.com>
2020-09-18 14:09:16 +0530

    added retry function

--- main.py
+++ main.py
@@ -3,61 +3,49 @@ import os
 import time
 
 
-ET_path = r'C:\Program Files (x86)\Caterpillar\Electronic Technician\Etech.exe'  # type:  str
-Active_codes = r'act_code.png'                                                # type: str
-Active_events = r'Active_event.png'                                              # type: str
-connect = r'connect.png'                                                                    # type: str
+ET_path = r'C:\Program Files (x86)\Caterpillar\Electronic Technician\Etech.exe'
+Active_codes = r'act_code.png'
+Active_events = r'Active_event.png'
+connect = r'connect.png'
 ok = r'ok.png'
 check = r'check_box.png'
 ECM_select = r'HVAC_ECM.png'
 log = r'log_code.png'
-delay = 5
 
 
 def open_application(app_path):
     os.startfile(app_path)
     print("app_opened")
-    #time.sleep(2*delay)
-    #pyautogui.hotkey('winleft')
 
 
 def click_on_button(image_path):
     position = pyautogui.locateCenterOnScreen(image_path)
+    for i in range(0, 5):
+        if position is None:
+            position = pyautogui.locateCenterOnScreen(image_path)
     print(position)
     pyautogui.moveTo(position)
-    pyautogui.click()
-    pyautogui.click()
-    pyautogui.click()
-
+    for i in range(0, 2):
+        pyautogui.click()
+        print("button_clicked")
     return position
 
 
 def take_screenshot(position):
-    img = pyautogui.screenshot(region=(0, 0, 300, 400))
+    img = pyautogui.screenshot(region=(0, 0, 800, 600))
 
 
-'''
-if __name__ == "__main__":
+open_application(ET_path)
+time.sleep(10)
 
-open_alication(ET_path)
-time.sleep(2.5*delay)
 connect_position = click_on_button(connect)
-print("connect_clicked")
-time.sleep(8*delay)
-connect_position = click_on_button(ECM_select)
-print("ECM_Selected")
-time.sleep(1*delay)
-connect_position = click_on_button(ok)
-print("ok_clicked")
-time.sleep(2*delay)
-code_pos = click_on_button(Active_codes)
-print("code_clicked")
-time.sleep(2*delay)
+time.sleep(100)
 
-code_pos = click_on_button(log)
-print(type(code_pos))
-print("check_clicked")
-'''
-os.remove(r"C:\Projects\UAT\AD63\Test_results\images\result.png")
+select_position = click_on_button(ECM_select)
+time.sleep(5)
 
+ok_position = click_on_button(ok)
+time.sleep(10)
 
+code_position = click_on_button(Active_codes)
+time.sleep(5)
